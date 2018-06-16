6e0b2e3778422b57cd4a18d99983cf87d7c4217a
TERiSM7 <terism7@gmail.com>
2018-06-16 10:39:58 +0700

    Git init

---
+++ pages/hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <div>
+        <app-logo/>
+        <div>
+          <hello></hello>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Hello from '~/components/Hello.vue'
+import AppLogo from '~/components/AppLogo.vue'
+export default {
+  components: {
+    Hello,
+    AppLogo
+  }
+}
+
+</script>
